9b71bac77abe5dc7205e33fb33131e0b95493860
Paul Villanueva <pmvillanueva@gmail.com>
2017-01-16 10:26:44 -0800

    second push

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 $(function() {
-  console.log("hello world");
+  $(".js-event-form").submit(function(event){
+    event.preventDefault();
+
+  var sportsEvent = $("#sports-event").val();
+  var date = $("#date").val();
+  var location = $("#location").val();
+
+  $(".events").append(sportsEvent, date, location);
+
+
+  $("#sports-event").val("");
+  $("#date").val("");
+  $("#location").val("");
+
+  });
+
+
 });
