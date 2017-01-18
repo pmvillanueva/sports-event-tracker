477bacdbb48f6d9e4568895c9b4a1be1b3e99078
Paul Villanueva <pmvillanueva@gmail.com>
2017-01-18 00:26:59 -0800

    third push

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
   $(".js-event-form").submit(function(event){
     event.preventDefault();
 
@@ -7,6 +7,21 @@ $(function() {
   var location = $("#location").val();
 
   $(".events").append(sportsEvent, date, location);
+
+//$("#event-results").append($("<li>").text(sportsEvent));
+//$("#event-results").append($("<li>").text(date));
+//$("#event-results").append($("<li>").text(location));
+
+/*function myFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = sportsEvent;
+    cell2.innerHTML = date;
+    cell3.innerHTML = location;
+}*/
 
 
   $("#sports-event").val("");
